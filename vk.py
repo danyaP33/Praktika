44b0650fe5b036651b6c817cfd813e78fe1b56e6
danyaP33 <115029382+danyaP33@users.noreply.github.com>
2023-06-07 15:05:48 +0300

    Add files via upload

--- vk.py
+++ vk.py
@@ -15,22 +15,18 @@ vk = vk_session.get_api()
 posts = vk.wall.get(owner_id='-72687742', count=count)['items']
 all_posts = []
 
-while offset < 100:
+while offset < 1000:
     with open('volnc_comments.csv', 'w', encoding='utf-8') as file:
         a_pen = csv.writer(file)
         a_pen.writerow(('id', 'post_id', 'from_id', 'text'))
         for post in posts:
-            comments = vk.wall.getComments(owner_id='-72687742', post_id=post['id'], count=1000)['items']
-            all_comments = []
-            while offset < 100:
-                for comment in comments:
-                    try:
-                        a_pen.writerow((comment['id'], comment['post_id'], comment['from_id'], comment['text']))
-                    except:
-                        pass
-                data2 = json.loads(json.dumps(comments))
-                all_comments.extend(data2)
-                offset += 100
+            comments = vk.wall.getComments(owner_id='-72687742', post_id=post['id'], count=count, offset=offset)['items']
+            for comment in comments:
+                try:
+                    a_pen.writerow((comment['id'], comment['post_id'], comment['from_id'],
+                                    comment['text']))
+                except:
+                    pass
 
     data = json.loads(json.dumps(posts))
     offset += 100
