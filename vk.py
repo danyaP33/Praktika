66685893e9747d3410df9fa7a49063771a432a64
danyaP33 <115029382+danyaP33@users.noreply.github.com>
2023-06-07 14:27:21 +0300

    Add files via upload

--- vk.py
+++ vk.py
@@ -22,14 +22,16 @@ while offset < 100:
         for post in posts:
             comments = vk.wall.getComments(owner_id='-72687742', post_id=post['id'], count=1000)['items']
             all_comments = []
-while offset < 100:
-    for comment in comments:
-        try:
-            a_pen.writerow((comment['id'], comment['post_id'], comment['from_id'], comment['text']))
-        except:
-            pass
-    data2 = json.loads(json.dumps(comments))
-    all_comments.extend(data2)
+            while offset < 100:
+                for comment in comments:
+                    try:
+                        a_pen.writerow((comment['id'], comment['post_id'], comment['from_id'], comment['text']))
+                    except:
+                        pass
+                data2 = json.loads(json.dumps(comments))
+                all_comments.extend(data2)
+                offset += 100
+
     data = json.loads(json.dumps(posts))
     offset += 100
     all_posts.extend(data)
