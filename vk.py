06a77c7513b3fa4fd08c21e00cf024b7253657bc
danyaP33 <115029382+danyaP33@users.noreply.github.com>
2023-06-14 22:52:58 +0300

    Add files via upload

--- vk.py
+++ vk.py
@@ -1,36 +1,28 @@
 import json
-import vk
 import vk_api
 import csv
 import time
 import tok
 import requests
 
-
-# def get_comm():
 vk_session = vk_api.VkApi(token=tok.t)
 count = 100
 offset = 0
 vk = vk_session.get_api()
-posts = vk.wall.get(owner_id='-72687742', count=count)['items']
-all_posts = []
 
-while offset < 1000:
-    with open('volnc_comments.csv', 'w', encoding='utf-8') as file:
-        a_pen = csv.writer(file)
-        a_pen.writerow(('id', 'post_id', 'from_id', 'text'))
+with open('volnc_commentss.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(('id', 'post_id', 'from_id', 'text'))
+
+    while offset < 1000:
+        posts = vk.wall.get(owner_id='-72687742', count=count, offset=offset)['items']
+
         for post in posts:
-            comments = vk.wall.getComments(owner_id='-72687742', post_id=post['id'], count=count, offset=offset)['items']
+            comments = vk.wall.getComments(owner_id='-72687742', post_id=post['id'], count=count)['items']
             for comment in comments:
-                try:
-                    a_pen.writerow((comment['id'], comment['post_id'], comment['from_id'],
-                                    comment['text']))
-                except:
-                    pass
+                writer.writerow((comment['id'], comment['post_id'], comment['from_id'], comment['text']))
 
-    data = json.loads(json.dumps(posts))
-    offset += 100
-    all_posts.extend(data)
-    time.sleep(1)
+        offset += count
+        time.sleep(1)
 
 print(1)
