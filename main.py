53f9fb9f70492a3f7568e490559df99bfcbd2478
danyaP33 <115029382+danyaP33@users.noreply.github.com>
2023-06-15 13:46:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,10 @@
 import csv
-import datetime
 import time
-import vk
-import vk_api
 import requests
 import tok
 
-
-def get_comments():
-    t = tok.t
-    version = 5.131
-    owner_id = '-72687742'
-    post_id = '8272'
-    count = 100
-    offset = 0
-    sort = 'asc'
-    all_comments = []
-
-    while offset < 100:
-        response = requests.get('https://api.vk.com/method/wall.getComments',
-                                params={
-                                    'access_token': t,
-                                    'v': version,
-                                    'owner_id': owner_id,
-                                    'post_id': post_id,
-                                    'count': count,
-                                    'offset': offset,
-                                    'sort': sort
-                                }
-                                )
-        data = response.json()['response']['items']
-        offset += 100
-        all_comments.extend(data)
-        time.sleep(1)
-
-    return all_comments
-
-
-def file_writer_comments(data):
-    with open('volnc_comments.csv', 'w', encoding='utf-8') as file:
-        a_pen = csv.writer(file)
-        a_pen.writerow(('id', 'post_id', 'date', 'from_id', 'text'))
-        for comments in data:
-            try:
-                a_pen.writerow((comments['id'], comments['post_id'], comments['date'], comments['from_id'], comments['text']))
-            except:
-                pass
-
-all_comments = get_comments()
-
-file_writer_comments(all_comments)
-
-
 def get_posts():
-    token = '02f8dab702f8dab702f8dab7be01ecf55c002f802f8dab76687df60128d177b70813cb1'
+    token = tok.t
     version = 5.131
     owner_id = '-72687742'
     count = 100
